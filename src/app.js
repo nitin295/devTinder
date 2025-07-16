20497407591c3cbc1918adce51b5d8344092bf07
Nitin Choudhary <charynitin454@gmail.com>
2025-07-17 01:40:01 +0530

    routing changes

--- src/app.js
+++ src/app.js
@@ -3,9 +3,20 @@ const express = require('express');
 
 const app = express();
 
+//Request Handler
+// app.use('/',(req,res)=>{
+//     res.send("server is running on 3000");
+// })
 
-app.use((req,res)=>{
-    res.send("server is running on 3000");
+
+app.get('/user', (req,res)=>{
+    res.send({
+        firstname:"Nitin"
+    })
+})
+
+app.post('/user', (req,res)=>{
+    res.send('data stored-------')
 })
 
 
