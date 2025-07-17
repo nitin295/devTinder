da36839c8d902301d1431bd8e469cf652dbf1def
Nitin Choudhary <charynitin454@gmail.com>
2025-07-17 19:13:57 +0530

    Error Handling

--- src/app.js
+++ src/app.js
@@ -4,11 +4,25 @@ const app = express();
 
 
 app.use('/admin',adminAuth, (req,res)=>{
-    res.send('Admin is loggedIn--')
+    // throw new Error("--------");
+
+    try {
+        res.send('Admin is loggedIn--');
+    } catch (error) {
+        res.status(500).send("Ther is some error---")
+    }
+    
 })
 
 app.use('/user', userAuth, (req,res)=>{
     res.send("Get all the data of users----")
+})
+
+
+app.use('/',(err, req, res, next)=>{
+    if (err){
+        res.status(500).send("Something wents wrong")
+    }
 })
 
 
