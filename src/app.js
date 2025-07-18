6a69b63aa75ab6889e6e7f960a336dd891482fc8
Nitin Choudhary <charynitin454@gmail.com>
2025-07-19 00:58:32 +0530

    Get:/getUsers and Get:/userFeeds

--- src/app.js
+++ src/app.js
@@ -6,27 +6,52 @@ const User = require('./models/user')
 const app = express();
 
 app.use(express.json());
-
+// Post: for sign Up to User--
 app.post('/signup', async (req, res) => {
-
     // creating new instance for user model 
     const user = new User(req.body);    
-    // const user = new User({
-    //     firstName: 'Nitin',
-    //     lastName: 'Choudhary',
-    //     email: 'nitinchoudhary0306@gmail.com',
-    //     password: 'Nitin@1996',
-    //     gender: 'Male'
-    // });
-
     try {
         await user.save()
         res.send('User data is saved successfull')   
     } catch (error) {
         res.status(400).send("Error in signup" + error.message)
     }
-})
+});
 
+
+// Get: single User behalf of User.emaliId
+app.get('/getUsers', async (req,res) => {
+    const userEmail = req.body.emailId;
+    try {
+        const users = await User.find({ emailId: userEmail});   
+        if(users.length === 0){
+            res.status(404).send('User not found');
+        } 
+        else {
+            res.send(users);
+        }
+    } catch (error) {
+       res.status(401).send('Something wents wrongs') 
+    }
+});
+
+// Get All the Users
+app.get('/userFeeds', async (req,res) => {
+    try {
+        const allUsers = await User.find({});
+        if(!allUsers){
+            res.status(404).send('Not get all users:Something wents wrong')
+        }
+        else{
+            res.send(allUsers)
+        }
+    } catch (error) {
+        res.status(404).send('Something wents wrong')
+    }
+});
+
+
+// Server is listing on 3000--
 connectDB()
 .then(() => {
     console.log('Connected to the database--');
