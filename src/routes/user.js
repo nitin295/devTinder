ac5e6b6d0bbc965b1dadb53f60adab1bb80e4872
Nitin Choudhary <charynitin454@gmail.com>
2025-07-23 00:13:45 +0530

    accept requast: Post

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user')
+const User = require('../models/user');
+const { userAuth } = require('../middlewares/auth');
+const ConnectionRequestModel = require('../models/connectionRequest');
+
+
 
 // Get: single User behalf of User.emaliId
 router.get('/getUsers', async (req,res) => {
@@ -21,7 +25,11 @@ router.get('/getUsers', async (req,res) => {
 // Get: all the Users
 router.get('/userFeeds', async (req,res) => {
     try {
-        const allUsers = await User.find({});
+        const pageNum = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const skip = (pageNum - 1) * limit;
+
+        const allUsers = await User.find({}).skip(skip).limit(limit);
         if(!allUsers){
             res.status(404).send('Not get all users:Something wents wrong')
         }
@@ -71,6 +79,63 @@ router.patch('/updateUser/:userId', async (req,res) => {
         res.status(200).send('user is updated successfully')
     } catch (error) {
         res.status(400).send('Something went wrong')
+    }
+})
+
+router.get('/user/request/received', userAuth, async (req,res) =>{
+    try {
+        const loggedInUser = req.user;
+        const connectionRequest = await ConnectionRequestModel.find({
+            toUserId: loggedInUser._id,
+            status: 'interested'
+        }).populate('formUserId',['firstName,lastName']);
+        
+        res.json({
+            status: 200,
+            data: connectionRequest
+        })
+    } catch (error) {
+     res.json({
+        status: 401,
+        message: "Not get user"
+     })   
+    }
+})
+
+router.get('/connection/feed', userAuth, async (req,res) => {
+    try {
+        const loggedInUser = req.user;
+        const pageNum = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const skip = (pageNum - 1) * limit;
+
+        const connectionRequest = await ConnectionRequestModel.find({
+            $or: [
+                [
+                    { fromUserId: loggedInUser._id },
+                    { toUserId: loggedInUser._id }
+            ]
+        ]
+        }).select('fromUserId toUserId');
+
+        const hideFeeduser = new set();
+        connectionRequest.forEach((request)=>{
+            hideFeeduser.add(req.fromUserId.toString())
+            hideFeeduser.add(req.toUserId.toString())
+        });
+        const user = User.find({
+         $and:  [
+            {_id: { $nin: Array.from(hideFeeduser)}},
+            { _id:{ $ne: loggedInUser._id}}
+        ]
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+        
+        res.send(user)
+    } catch (error) {
+       res.json({
+        status: 401,
+        message: 'Something wents wrong' + error.message
+       }) 
     }
 })
 
